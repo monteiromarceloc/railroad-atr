ffe8e7bf05129e958a9858ba58758b348350bab2
Marcelo Monteiro <monteiromarceloc@gmail.com>
2019-11-20 22:11:53 -0800

    folders structure

---
+++ Railroad/Railroad.cpp
@@ -0,0 +1,217 @@
+﻿/*********************************************************************************
+*
+*  TRABALHO FINAL AUTOMAÇÃO EM TEMPO REAL - ELT012
+*  CAMILA MARTINS E MARCELO MONTEIRO
+*
+**********************************************************************************/
+
+#define WIN32_LEAN_AND_MEAN 
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <process.h>						//_beginthreadex() e _endthreadex()
+#include <conio.h>							//_getch
+#include "CheckForError.h"
+
+using namespace std;
+
+#define WHITE   FOREGROUND_RED   | FOREGROUND_GREEN | FOREGROUND_BLUE
+#define HLGREEN FOREGROUND_GREEN | FOREGROUND_INTENSITY
+#define HLRED   FOREGROUND_RED   | FOREGROUND_INTENSITY
+
+typedef unsigned (WINAPI* CAST_FUNCTION)(LPVOID);	//Casting para terceiro e sexto parâmetros da função
+													//_beginthreadex
+typedef unsigned* CAST_LPDWORD;
+
+#define NUM_THREADS_READ_REMOTE 2
+#define	ESC	0x1B									// Tecla para encerrar o programa
+
+DWORD WINAPI ThreadReadRemote(int);
+//DWORD WINAPI ThreadPopMessage(int);
+//DWORD WINAPI ThreadHotWheel(int);
+//DWORD WINAPI ThreadShowData(int);
+//DWORD WINAPI ThreadShowAlarms(int);
+//DWORD WINAPI Thread(int);
+
+HANDLE hMutex;							// Mutex base
+HANDLE hSem;					        // Semáforo base
+HANDLE hEventBlockRead;					// Evento de sinalização de bloqueio da tarefa de leitura
+HANDLE hEventEnd;						// Evento de sinalização de término
+HANDLE hEventTime;						// Evento para temporizadores tmeout (nunca será sinalizado)
+HANDLE hOut;							// Handle para a saída da console
+
+typedef struct {						// Estrutura de dados para a lista circular
+	char conteudo[41];
+}Dados;
+
+Dados Mensagens[200];								// Lista circular em memória
+HANDLE hMutexNSEQ;							// Mutex para NSEQ
+int NSEQ;
+SYSTEMTIME timestamp;
+
+int nTecla;								//Variável que armazena a tecla digitada para sair
+
+void gerarAlfaNumAleatorio(char* alfa, int len);
+
+// THREAD PRIMÁRIA
+int main() {
+	HANDLE hThreadsRead[NUM_THREADS_READ_REMOTE];
+	HANDLE hThreadPop, hThreadHotWheel, hThreadShowData, hThreadShowAlarms;
+	DWORD dwIdRR, dwIdPop, dwIdHW, dwIdSD, dwIdSA;
+	DWORD dwExitCode = 0;
+	DWORD dwRet;
+	int i;
+
+	// --------------------------------------------------------------------------
+	// Obtém um handle para a saída da console
+	// --------------------------------------------------------------------------
+
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hOut == INVALID_HANDLE_VALUE)
+		SetConsoleTextAttribute(hOut, WHITE);
+		printf("Erro ao obter handle para a saída da console\n");
+
+	// --------------------------------------------------------------------------
+	//  Cria objetos de sincronização
+	// --------------------------------------------------------------------------
+
+	hEventBlockRead = CreateEvent(NULL, TRUE, FALSE, "BlockEvento"); //Evento de reset manual
+	CheckForError(hEventBlockRead);
+	hEventEnd = CreateEvent(NULL, TRUE, FALSE, "EscEvento"); //Evento de reset manual
+	CheckForError(hEventEnd);
+	hMutexNSEQ = CreateMutex(NULL, FALSE, "NumSequencial");
+	CheckForError(hMutexNSEQ);
+
+	// --------------------------------------------------------------------------
+	// Criação de threads
+	// --------------------------------------------------------------------------
+
+	// Threads Leitura
+	for (i = 0; i < NUM_THREADS_READ_REMOTE; ++i) {
+		hThreadsRead[i] = (HANDLE)_beginthreadex(
+			NULL,
+			0,
+			(CAST_FUNCTION)ThreadReadRemote,	//Casting necess�rio
+			(LPVOID)i,
+			0,
+			(CAST_LPDWORD)&dwIdRR);		//Casting necess�rio
+		if (hThreadsRead[i]) {
+			SetConsoleTextAttribute(hOut, WHITE);
+			printf("Thread leitura %d criado com Id=%0x\n", i, dwIdRR);
+		}
+		else {
+			SetConsoleTextAttribute(hOut, WHITE);
+			printf("Erro na criacao da thread leitura! N = %d Erro = %d\n", i, errno);
+			exit(0);
+		}
+	}//for
+
+
+	// --------------------------------------------------------------------------
+	// Leitura do teclado
+	// --------------------------------------------------------------------------
+
+	do {
+		SetConsoleTextAttribute(hOut, WHITE);
+		printf("Tecle <Esc> para terminar\n");
+		nTecla = _getch();
+	} while (nTecla != ESC);
+
+	// --------------------------------------------------------------------------
+	// Aguarda término das threads e encerra programa
+	// --------------------------------------------------------------------------
+
+	dwRet = WaitForMultipleObjects(NUM_THREADS_READ_REMOTE, hThreadsRead, TRUE, INFINITE);
+	CheckForError(dwRet == WAIT_OBJECT_0);
+
+	// Fecha todos os handles de objetos do kernel
+	for (int i = 0; i < NUM_THREADS_READ_REMOTE; ++i)
+		CloseHandle(hThreadsRead[i]);
+	//for
+
+	CloseHandle(hMutexNSEQ);
+	CloseHandle(hEventEnd);
+	CloseHandle(hEventBlockRead);
+
+	SetConsoleTextAttribute(hOut, WHITE);
+	return EXIT_SUCCESS;
+
+}//main
+
+DWORD WINAPI ThreadReadRemote(int i) {
+
+	SetConsoleTextAttribute(hOut, HLRED);
+	printf("Thread de leitura %d iniciando execucao...\n", i);
+
+	char auxMensagem[41];
+	DWORD status, ret;
+	LONG dwContagemPrevia;
+	int REMOTA,DIAG,ID_PARTE2,ESTADO,TIMESTAMP;
+	char ID_PARTE1[3];
+
+	GetLocalTime(&timestamp);
+
+	do {
+
+		// temporizador
+		hEventTime = CreateEvent(NULL, TRUE, FALSE, "EvTimeOut");
+		status = WaitForSingleObject(hEventTime, 500);
+		if (status == WAIT_TIMEOUT) {
+
+			// gerar mensagem
+			WaitForSingleObject(hMutexNSEQ, INFINITE);
+			NSEQ++;
+			if (NSEQ == 1000000) NSEQ = 1;
+			ReleaseMutex(hMutexNSEQ);
+
+			REMOTA = (rand() % 2);
+			DIAG = (rand() % 1000);
+			gerarAlfaNumAleatorio(ID_PARTE1, 3);
+			ID_PARTE2 = (rand() % 1000);
+			ESTADO = (rand() % 2);
+			sprintf(auxMensagem, "%07d;55;%02d;%03d;%s-%04d;%d;%02d:%02d:%02d", NSEQ, REMOTA, DIAG, ID_PARTE1, ID_PARTE2, ESTADO, timestamp.wHour, timestamp.wMinute, timestamp.wSecond);
+			SetConsoleTextAttribute(hOut, HLRED);
+			printf("Thread Leitura %d gerou a mensagem: %s.\n", i, auxMensagem);
+
+			// verificar se há posição livre na lista
+			// bloquear-se ou
+			// adicionar à lista
+
+		} else{
+			// não deve cair aqui.
+			SetConsoleTextAttribute(hOut, HLRED);
+			printf("Erro no temporizador da Thread Leitura %d.\n", i);
+		}
+	} while (nTecla != ESC);
+
+	SetConsoleTextAttribute(hOut, HLRED);
+	printf("Thread Leitura %d encerrando execucao.\n", i);
+	_endthreadex(0);
+	return(0);
+}//ThreadFilhote
+
+//função que gera numeros inteiros aleatorios
+int gerarNumeroAleatorioInteiro(int digit) {
+	int y = pow(10, digit);
+	int x = rand() % y;
+	return x;
+}
+
+//função que gera numeros reais aleatorios 
+float gerarNumeroAleatorioReal(int digit) {
+	float y = pow(10.0, digit - 2);
+	float x = fmod(rand(), (y));
+	float z = (rand() % 10) / 10.0;
+	x = x + z;
+	return x;
+}
+
+//função que gera sequencias alfanumericas aleatorias
+void gerarAlfaNumAleatorio(char* alfa, int len) {
+	char charSet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (int i = 0; i < len; i++) {
+		alfa[i] = charSet[rand() % (sizeof(charSet) - 1)];
+	}
+	alfa[len] = 0;
+}
---
+++ Teclado/Teclado.cpp
@@ -0,0 +1,135 @@
+#define WIN32_LEAN_AND_MEAN 
+#define _CRT_SECURE_CPP_OVERLOAD_STANDARD_NAMES 1
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <process.h>	// _beginthreadex() e _endthreadex() 
+#include <conio.h>		// _getch
+
+#include <iostream>
+using namespace std;
+
+#define h			0x68
+#define r			0x72
+#define s			0x73
+#define a			0x61
+#define	ESC			0x1B
+
+HANDLE h1Event;			// Handle para Evento Leitura Remota 1 on/off
+HANDLE h2Event;			// Handle para Evento Leitura Remota 2 on/off
+HANDLE hhEvent;			// Handle para Evento Leitura dos Detectores de Roda Quente on/off
+HANDLE hrEvent;			// Handle para Evento Retirada de Mensagens on/off
+HANDLE hsEvent;			// Handle para Evento Exibicao de Dados de Sinalização Ferroviária on/off
+HANDLE haEvent;			// Handle para Evento Exibicao de Alarmes on/off
+HANDLE hEscEvent;		// Handle para Evento Encerrar demais tarefas
+
+int main() {
+
+	int nTecla;
+
+	STARTUPINFO si;					// StartUpInformation para novo processo 
+	PROCESS_INFORMATION NewProcess, DadosProcess, OPProcess, AlarmeProcess;	// Informações sobre os novos processos criados 
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);				// Tamanho da estrutura em bytes
+
+	h1Event = CreateEvent(NULL, TRUE, FALSE, "Leitura1ON-OFF");
+	h2Event = CreateEvent(NULL, TRUE, FALSE, "Leitura2ON-OFF");
+	hhEvent = CreateEvent(NULL, TRUE, FALSE, "LeituraDetectoresON-OFF");
+	hrEvent = CreateEvent(NULL, TRUE, FALSE, "RetiradaMensagensON-OFF");
+	hsEvent = CreateEvent(NULL, TRUE, FALSE, "ExibicaoDadosON-OFF");
+	haEvent = CreateEvent(NULL, TRUE, FALSE, "ExibicaoAlarmesON-OFF");
+	hEscEvent = CreateEvent(NULL, TRUE, FALSE, "EncerraTarefas");
+
+	CreateProcess(
+		"..\\Debug\\Controle De Dados.exe",
+		NULL,	// linha de comando
+		NULL,	// atributos de segurança: Processo
+		NULL,	// atributos de segurança: Thread
+		FALSE,	// herança de handles
+		//CREATE_NEW_CONSOLE,	
+		NORMAL_PRIORITY_CLASS,	// CreationFlags
+		NULL,	// lpEnvironment
+		"..\\Controle de Dados",
+		&si,			// lpStartUpInfo
+		&NewProcess);	// lpProcessInformation
+
+	CreateProcess(
+		"..\\Debug\\Exibicao de Dados.exe",
+		NULL,	// linha de comando
+		NULL,	// atributos de segurança: Processo
+		NULL,	// atributos de segurança: Thread
+		FALSE,	// herança de handles
+		CREATE_NEW_CONSOLE,
+		NULL,	// lpEnvironment
+		"..\\Exibicao de Dados",
+		&si,			// lpStartUpInfo
+		&DadosProcess);	// lpProcessInformation
+
+	CreateProcess(
+		"..\\Debug\\Exibicao de OPs.exe",
+		NULL,	// linha de comando
+		NULL,	// atributos de segurança: Processo
+		NULL,	// atributos de segurança: Thread
+		FALSE,	// herança de handles
+		CREATE_NEW_CONSOLE,
+		NULL,	// lpEnvironment
+		"..\\Exibicao de Ops",
+		&si,			// lpStartUpInfo
+		&OPProcess);	// lpProcessInformation
+
+	CreateProcess(
+		"..\\Debug\\Exibicao de Alarmes.exe",
+		NULL,	// linha de comando
+		NULL,	// atributos de segurança: Processo
+		NULL,	// atributos de segurança: Thread
+		FALSE,	// herança de handles
+		CREATE_NEW_CONSOLE,
+		NULL,	// lpEnvironment
+		"..\\Exibicao de Alarmes",
+		&si,			// lpStartUpInfo
+		&AlarmeProcess);	// lpProcessInformation
+
+	HANDLE EndProcess[4] = { NewProcess.hProcess, DadosProcess.hProcess, OPProcess.hProcess, AlarmeProcess.hProcess };
+
+	do {
+		printf("Tecle uma acao valida para gerar evento ou <Esc> para terminar\n");
+		nTecla = _getch();
+		if (nTecla == 1) SetEvent(h1Event);			// Gera 1 evento
+		if (nTecla == 2) SetEvent(h2Event);			// Gera 1 evento
+		if (nTecla == h) SetEvent(hhEvent);			// Gera 1 evento
+		if (nTecla == r) SetEvent(hrEvent);			// Gera 1 evento
+		if (nTecla == s) SetEvent(hsEvent);			// Gera 1 evento
+		if (nTecla == a) SetEvent(haEvent);			// Gera 1 evento
+		else if (nTecla == ESC) SetEvent(hEscEvent);	// Termina processos
+	} while (nTecla != ESC);
+
+	WaitForMultipleObjects(4, EndProcess, TRUE, INFINITE);
+
+	CloseHandle(h1Event);
+	CloseHandle(h2Event);
+	CloseHandle(hhEvent);
+	CloseHandle(hrEvent);
+	CloseHandle(hsEvent);
+	CloseHandle(haEvent);
+	CloseHandle(hEscEvent);
+
+	CloseHandle(NewProcess.hProcess);
+	CloseHandle(NewProcess.hThread);
+
+	CloseHandle(DadosProcess.hProcess);
+	CloseHandle(DadosProcess.hThread);
+
+	CloseHandle(OPProcess.hProcess);
+	CloseHandle(OPProcess.hThread);
+
+	CloseHandle(AlarmeProcess.hProcess);
+	CloseHandle(AlarmeProcess.hThread);
+
+	printf("\nAcione uma tecla para terminar\n");
+	_getch();
+
+	return EXIT_SUCCESS;
+
+}
